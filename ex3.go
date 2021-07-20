278ac76222cacb01da7bc32073434417385d7a6c
MananKumawat <87689251+MananKumawat@users.noreply.github.com>
2021-07-20 20:03:27 +0530

    ex3.go

--- ex3.go
+++ ex3.go
@@ -1,36 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type Employee interface { // Interface for all Employee having common method salary
-	salary() int
+	salary() float64
 }
 
-type Worker struct{ // Struct for Fulltime and Contractor
-	basic int
-	duration string
+type Fulltime struct{ // Struct for Fulltime
+	basic float64
+	NumberOfMonths int
+}
+
+type Contractor struct{ // Struct for Contractor
+	basic float64
+	NumberOfMonths int
 }
 
 type Freelencer struct{ // Struct for Freelencer
-	basic int
-	duration string
+	basic float64
+	NumberOfDays int
 }
 
-func (w Worker) salary() int { // Salary for Worker
-	return 28 * w.basic
+func (w Contractor) salary() float64 { // Salary for Contractor
+	return 28.0 * w.basic * float64(w.NumberOfMonths)
 }
 
-func (f Freelencer) salary() int { // Salary for Freelencer
-	return 10 * 28 * f.basic
+func (w Fulltime) salary() float64 { // Salary for Fulltime
+	return 28.0 * w.basic * float64(w.NumberOfMonths)
+}
+
+func (f Freelencer) salary() float64 { // Salary for Freelencer
+	return 8.0 * f.basic * float64(f.NumberOfDays)
+}
+
+func TotalExpenses(employees []Employee) float64 { // Calculating Total Expenses
+	var total float64 = 0
+	for _, employee := range employees {
+		total += employee.salary()
+	}
+	return total
 }
 
 func main () {
-	fulltime := Worker{500, "daily"}
-	contractor := Worker{100, "daily"}
-	freelancer := Freelencer{10, "hourly"}
+	fulltime := Fulltime{500, 10}
+	contractor := Contractor{100, 8}
+	freelancer := Freelencer{10, 28}
 
 	var employees = []Employee{fulltime, contractor, freelancer}
-	for _, employee := range employees {
-		fmt.Println(employee.salary())
-	}
+
+	fmt.Println(TotalExpenses(employees))
 }
