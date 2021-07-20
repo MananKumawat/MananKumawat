ef72c9dca091f0be17db0c75d7e330da9cf015ad
MananKumawat <87689251+MananKumawat@users.noreply.github.com>
2021-07-20 21:00:28 +0530

    ex3.go

--- ex3.go
+++ ex3.go
@@ -1,82 +1,54 @@
 package main
 
 import (
-	"encoding/json"
-	"errors"
 	"fmt"
-	"log"
 )
 
-type Matrix struct{
-	rows int `json:"rows"`
-	columns int `json:"columns"`
-	elements [][]int `json:"elements"`
+type Employee interface { // Interface for all Employee having common method salary
+	salary() float64
 }
 
-func MatrixInit(row, column int) Matrix { // Matrix Initialization
-	var matrix Matrix
-	matrix.rows = row
-	matrix.columns = column
-	matrix.elements = make([][]int, row)
-	for i:=0; i<row; i++{
-		matrix.elements[i] = make([]int, column)
+type Fulltime struct{ // Struct for Fulltime
+	basic float64
+	NumberOfMonths int
+}
+
+type Contractor struct{ // Struct for Contractor
+	basic float64
+	NumberOfMonths int
+}
+
+type Freelencer struct{ // Struct for Freelencer
+	basic float64
+	NumberOfDays int
+}
+
+func (w Contractor) salary() float64 { // Salary for Contractor
+	return 28.0 * w.basic * float64(w.NumberOfMonths)
+}
+
+func (w Fulltime) salary() float64 { // Salary for Fulltime
+	return 28.0 * w.basic * float64(w.NumberOfMonths)
+}
+
+func (f Freelencer) salary() float64 { // Salary for Freelencer
+	return 8.0 * f.basic * float64(f.NumberOfDays)
+}
+
+func TotalExpenses(employees []Employee) float64 {
+	var total float64 = 0
+	for _, employee := range employees {
+		total += employee.salary()
 	}
-	return matrix
+	return total
 }
 
-func (matrix Matrix) GetNumberofRows() int { // Get number of rows of a matrix
-	return matrix.rows
+func main () {
+	fulltime := Fulltime{500, 10}
+	contractor := Contractor{100, 8}
+	freelancer := Freelencer{10, 28}
+
+	var employees = []Employee{fulltime, contractor, freelancer}
+
+	fmt.Println(TotalExpenses(employees))
 }
-
-func (matrix Matrix) GetNumberofColumns() int { // Get number of columns of a matrix
-	return matrix.columns
-}
-
-func (matrix *Matrix)  set(row, column, value int)  { // Set element on a particular position
-	if row<0 || column<0 || row>=matrix.rows || column>=matrix.columns {
-		err := errors.New("Index out of Bounds")
-		log.Fatal(err)
-	}
-	matrix.elements[row][column] = value
-}
-
-func (FirstMatrix Matrix) add(SecondMatrix Matrix) Matrix { // Add two matrices
-	row := FirstMatrix.rows
-	column := FirstMatrix.columns
-	FinalMatrix := MatrixInit(row, column)
-	for i:=0; i<row; i++ {
-		for j:=0; j<column; j++ {
-			FinalMatrix.elements[i][j] = FirstMatrix.elements[i][j] + SecondMatrix.elements[i][j]
-		}
-	}
-	return FinalMatrix
-}
-
-func (matrix Matrix) JsonMarshal() { // print in json format
-	m, err := json.Marshal(matrix.elements)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(string(m))
-}
-
-func main() {
-	matrix1 := MatrixInit(3, 3)
-	matrix2 := MatrixInit(3,3)
-	for i:=0; i<3; i++{
-		for j:=0; j<3; j++ {
-			matrix1.set(i, j, i+j)
-			matrix2.set(i, j, 2*(i+j))
-		}
-	}
-
-	matrix3 := matrix1.add(matrix2)
-
-	fmt.Println(matrix3.rows)
-	
-	matrix1.JsonMarshal()
-	matrix2.JsonMarshal()
-	matrix3.JsonMarshal()
-
-}
-
